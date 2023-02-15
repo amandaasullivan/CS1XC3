9701a2fb10fa00b17fa9712f0f3ead92c1d4d076
amandaasullivan <sullia8@mcmaster.ca>
2023-02-14 19:44:15 -0500

    Submitting Lab 4

--- L04/lab4.sh
+++ L04/lab4.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 # Use First Argument As Working Directory
-WDIR="PWD"
+WDIR="$1"
 filename="file1.txt"
-#cd "$WDIR"
+cd "$WDIR"
 
-echo "$1"
-echo "$2"
 
 #question 4
-if [ $# -gt 1 ] ; then
-    filename=$2
+if [ $# -gt 2 ] ; then
+    filename="$3"
 fi
 
 #question 1
-if [ $1 -eq 1 ] ; then
+if [ $2 -eq 1 ] ; then
     if [ ! -f "$filename" ] ; then
 	touch "$filename"
 	echo "$USER" >> "$filename"
     fi
 
-#question 2 
-elif [ $1 -eq 2 ] ; then
-    if [ -e "$filename" ] && [ ! -d "backup" ] ; then 
-	mkdir "backup"
+#question 2
+elif [ $2 -eq 2 ] ; then
+    if [ -f "$filename" ] ; then
+	if [ ! -d "backup" ] ; then
+		mkdir "backup"
+	fi
 	cp "./$filename" "./backup"
     fi
 
 #question 3
-elif [ $1 -eq 3 ] ; then
+elif [ $2 -eq 3 ] ; then
     if [ -f "backup/$filename" ] ; then
  	cp "backup/$filename" "./$filename"
 	rm "backup/$filename"
@@ -36,10 +36,10 @@ elif [ $1 -eq 3 ] ; then
 fi
 
 
-#question 5 
-if [ $# -gt 2 ] ; then
+#question 5
+if [ $# -gt 3 ] ; then
     touch "excess.txt"
-    for ((i=3 ; i <= $# ; i++)) ; do
+    for ((i=4 ; i <= $# ; i++)) ; do
 	echo "${!i}" >> "excess.txt" #${!1} is an indirect variable assignment
     done
 fi
