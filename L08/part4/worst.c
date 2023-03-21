cc9eeb6216c713d5d5b494c5d34d22f335db09f2
amandaasullivan <sullia8@mcmaster.ca>
2023-03-21 19:49:15 -0400

    commit lab 8

---
+++ L08/part4/worst.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+	//trigger the out of bounds array access warning
+	int arr[5] = { 1, 2, 3, 4, 5 };
+ 	int i = 5;
+ 	printf("%d\n",arr[i]);
+    	return 0;
+
+	//trigger the incrementing Boolean variable warning
+	bool b = true;
+	b++;
+	printf("b = %s",b);
+
+	//trigger the matching if and elese if conditions warning
+	int x = 2;
+   	if (x == 2) {
+        	printf("x is equal to 2");
+    	}
+    	else if (x == 2) {
+        	printf("x is also equal to 2");
+   	}
+
+	//trigger the zero division warning
+	int n = 6;
+	printf("6/0 is %d",num/0);
+
+
+    	return 0;
+}
