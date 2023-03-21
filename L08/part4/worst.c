48df0df97d687572eb8d6d863e73ea1ec609cdd3
amandaasullivan <sullia8@mcmaster.ca>
2023-03-21 19:59:46 -0400

    commit lab 8

--- L08/part4/worst.c
+++ L08/part4/worst.c
@@ -5,13 +5,13 @@ int main()
 {
 	//trigger the out of bounds array access warning
 	int arr[5] = { 1, 2, 3, 4, 5 };
- 	int i = 5;
+ 	int i = 6;
  	printf("%d\n",arr[i]);
     	return 0;
 
 	//trigger the incrementing Boolean variable warning
 	bool b = true;
-	b++;
+	b=b+1;
 	printf("b = %s",b);
 
 	//trigger the matching if and elese if conditions warning
@@ -25,7 +25,7 @@ int main()
 
 	//trigger the zero division warning
 	int n = 6;
-	printf("6/0 is %d",num/0);
+	printf("6/0 is %d",n/0);
 
 
     	return 0;
