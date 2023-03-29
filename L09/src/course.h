66ec386ca9c84d168703bcb676b3fcc680478763
amandaasullivan <sullia8@mcmaster.ca>
2023-03-28 20:58:39 -0400

    Committing L09 Part 5

--- L09/src/course.h
+++ L09/src/course.h
@@ -1,12 +1,19 @@
 #include "student.h"
 #include <stdbool.h>
+
+/**
+*@brief Holds data for course information including the course code and students enrolled
+*
+*This struct is used to store course information. It defines variables for student names, course codes, and the total number of students in a course. It also includes parameters for functions relating to course information that are used in the main file.
+*
+**/
  
 typedef struct _course 
 {
-  char name[100];
-  char code[10];
-  Student *students;
-  int total_students;
+  char name[100]; /**< The name of the course */
+  char code[10]; /**< The course code. */
+  Student *students; /**< A pointer to an array of Student objects representing the students enrolled in the course */
+  int total_students; /**< The totalnumber of students enrolled in the course. */
 } Course;
 
 void enroll_student(Course *course, Student *student);
--- L09/src/student.h
+++ L09/src/student.h
@@ -1,11 +1,17 @@
- 
+/**
+* @brief holds data for student information including student name, ID, grades
+*
+* This struct is used to store student information student first name, last name, id, and grades. This file also includes parameters for functions to add grades, generate GPA, and print student details which are used in the main file
+*
+**/
+
 typedef struct _student 
 { 
-  char first_name[50];
-  char last_name[50];
-  char id[11];
-  double *grades; 
-  int num_grades; 
+  char first_name[50]; /**< The first name of the student */
+  char last_name[50]; /**< The last name of the student */
+  char id[11]; /**< The student identification */
+  double *grades; /**< A memory address to an array of Grade objects representing the grades in the course. */
+  int num_grades; /**< The grade of the student */
 } Student;
 
 void add_grade(Student *student, double grade);
